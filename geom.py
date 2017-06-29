12438e68efe539dd7bfe03229e7c8acb8be64d8c
Nate-Wessel <bike756@gmail.com>
2017-06-29 13:36:57 +0100

    fixed line subsetting function

--- geom.py
+++ geom.py
@@ -3,7 +3,9 @@ from shapely.geometry import Point, LineString
 
 def subset_line(line,m1=0,m2=9**99):
 	"""subset a given linestring between two distances along 
-		its length. Returns a linestring."""
+		its length. Returns a linestring.
+		I'm pretty sure this is working, but I really ought to 
+		give it a good visual test!"""
 	# error checking
 	if not m1 >= 0 and m1 <= line.length:
 		raise SystemExit('invalid input for m1')
@@ -14,27 +16,31 @@ def subset_line(line,m1=0,m2=9**99):
 	# first cut at m1
 	subline = []
 	passed = False
-	for index, point in enumerate(list(line.coords)):
+	for index, xy in enumerate(list(line.coords)):
 		if not passed:
-			pm = line.project(Point(point))
+			pm = line.project(Point(xy))
 			if pm < m1:
 				continue
 			elif pm >= m1:
 				# we have passed m1 and will add it to the new line
-				subline.append(line.interpolate(m1))
+				p = line.interpolate(m1)
+				subline.append((p.x,p.y))
 				passed = True
 		elif passed: 
-			subline.append(point)
+			subline.append(xy)
 	# set the line to the new geometry cut at m1
 	line = LineString(subline) 
 	# now repeat for m2
+	m2 -= m1 # because this is opertaing on the already subsetted line
 	subline = []
-	for index, point in enumerate(list(line.coords)):
-		pm = line.project(Point(point))
+	for index, xy in enumerate(list(line.coords)):
+		pm = line.project(Point(xy))
 		# have we reached/passed m2 yet?
 		if pm >= m2:
-			subline.append(line.interpolate(m2))
-			return LineString(subline)
+			p = line.interpolate(m2)
+			subline.append((p.x,p.y))
+			line = LineString(subline)
+			return line
 		else: 
-			subline.append(point)
+			subline.append(xy)
 	raise SystemExit('we should not be here')
